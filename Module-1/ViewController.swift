029bf34169e37fe13ab75667bc2e7b72d7cdb10a
John Doe <sergey@itikho.com>
2024-10-27 14:05:23 +0300

    first commit

---
+++ Module-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module-1
+//
+//  Created by John Doe on 27.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
